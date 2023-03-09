fa4c72d53b1e22b94442ed5251a830edba7cc754
jannx999 <127385614+jannx999@users.noreply.github.com>
2023-03-09 08:59:19 +0100

    Create Aufg3.py

---
+++ Aufg3.py
@@ -0,0 +1,10 @@
+# 
+zahl1 = input  ("Hier zahl eingben:")
+zahl1 = int(zahl1)
+if zahl1 > 0 :
+    print("Zahl1 wird um 1 erhöht")
+    zahl1 = zahl1 + 1
+if zahl1 < 0 :
+    print("Zahl1 wird um 1 verringert")
+    zahl1 = zahl1 -1
+print("Zahl1 ist nun:", zahl1) 
