a6101c105ffd28004b33a87b6c9ac07f367fac14
jannx999 <127385614+jannx999@users.noreply.github.com>
2023-03-09 09:28:32 +0100

    Create Aufg5.py

---
+++ Aufg5.py
@@ -0,0 +1,12 @@
+# 
+zahl1 = int(input("Hier zahl eingben:"))
+if zahl1 >= 0 and zahl1 <= 20:
+    print("Die Zahl",zahl1,"liegt im ersten intervall von 0-20")
+if zahl1 >= 21 and zahl1 <= 40:
+    print("Die Zahl",zahl1,"liegt im zweiten intervall von 21-40")
+if zahl1 >= 41 and zahl1 <= 60:
+    print("Die Zahl",zahl1,"liegt im dritten intervall von 41-60")
+if zahl1 >= 61 and zahl1 <= 80:
+    print("Die Zahl",zahl1,"liegt im vierten intervall von 61-80")
+if zahl1 >= 81 and zahl1 <= 100:
+    print("Die Zahl",zahl1,"liegt im fünften intervall von 81-100")
